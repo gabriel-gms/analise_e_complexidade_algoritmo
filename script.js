2013d1a658f564d0bd29f734f5903ab26e700bfc
Gabriel Gomes <166407436+gabriel-gms@users.noreply.github.com>
2025-03-18 20:37:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,36 @@ function soma(n){
 }
 
 console.log("Soma recursiva: "+soma(5))
+
+function par(n){
+    if(n==0){
+        return true
+    } else if(n==1) {
+        return false
+    } else {
+        return impar(n-1)
+    }
+}
+function impar(n){
+    if(n==0){
+        return false
+    }
+    else if(n==1){
+        return true
+    }
+    else {
+        return par(n-1)
+    }
+}
+
+console.log("Recursão indireta, o numero e par?: "+par(10))
+
+function fibonacci(num){
+    if(num==1 || num==2){
+        return 1
+    }else{
+        return fibonacci(num-1)+fibonacci(num-2)
+    }
+}
+
+console.log("Recursão aninhada na sequecia fibonnaci: "+fibonacci(10))
